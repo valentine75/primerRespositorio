895233f37a05fed6760e64d520254e9883624266
Rufina75 <85413067+Rufina75@users.noreply.github.com>
2021-06-08 18:27:23 -0300

    Update índice.php

--- índice.php
+++ índice.php
@@ -1,7 +1,19 @@
-<header>
-<ol>
-<li> <a href="#">Home </a> </li>
-<li> <a href="#">Quines Somos</a> </li>
- <li> <a href="#">Contacto</a> </li>
-</ol>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+      <ol>
+      <li> <a href="#">Home </a> </li>
+      <li> <a href="#">Quines Somos</a> </li>
+      <li> <a href="#">Contacto</a> </li>
+      </ol>
 </header>
+</body>
+</html>
+
