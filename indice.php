439c9dd6125abcd4fb6f32e395829fd148c93741
Rufina Cruz <rufina_75@hotmail.com>
2021-06-09 23:44:06 -0300

    modificando localmente el archivo indice

---
+++ indice.php
@@ -0,0 +1,9 @@
+<header>
+      <ol>
+      <li> <a href="#">Home </a> </li>
+      <li> <a href="#">Quines Somos</a> </li>
+      <li> <a href="#">Contacto</a> </li>
+      <li> <a href="#">Ayuda</a> </li>
+      <li> <a href="#">Preguntas</a> </li>
+      </ol>
+</header>
--- índice.php
+++ índice.php
@@ -12,6 +12,8 @@
       <li> <a href="#">Home </a> </li>
       <li> <a href="#">Quines Somos</a> </li>
       <li> <a href="#">Contacto</a> </li>
+      <li> <a href="#">Ayuda</a> </li>
+      <li> <a href="#">Preguntas</a> </li>
       </ol>
 </header>
 </body>
